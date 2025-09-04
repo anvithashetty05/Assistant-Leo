db57f6ae5c39a4d544c1e278d72493703847862d
Anvitha Shetty <anvitha.jshetty5@gmail.com>
2025-09-04 09:08:34 +0530

    Initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "dusk till dawn": "https://www.youtube.com/watch?v=tt2k8PGm-TI",
+    "blinding lights": "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
+    "golden": "https://www.youtube.com/watch?v=P3cffdsEXXw",
+    "night changes": "https://www.youtube.com/watch?v=syFZfO_wfMQ"
+}
